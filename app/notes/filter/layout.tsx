935883861d3d6f3b0e1c770897507a2ddac63c67
Serhii_Paliienko <sergiopalienko@gmail.com>
2025-09-01 18:24:23 +0200

    fixed filter layout

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,15 +1,12 @@
 import type { ReactNode } from "react";
 import css from "./LayoutNotes.module.css";
-
-const FilterLayout = ({
-  children,
-  sidebar,
-  modal,
-}: {
+interface FilterLayoutProps {
   children: ReactNode;
-  sidebar: ReactNode;
-  modal: ReactNode;
-}) => {
+  sidebar?: ReactNode;
+  modal?: ReactNode;
+}
+
+const FilterLayout = ({ children, sidebar, modal }: FilterLayoutProps) => {
   return (
     <div className={css.container}>
       <aside className={css.sidebar}>{sidebar}</aside>
